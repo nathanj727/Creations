9767526561b7b82e3ec596424629293c2e32dc7c
Nathan <NathanJ.Burchfield@gmail.com>
2017-07-27 15:10:23 -0700

    Testing out somethings... 行不行 ？

---
+++ July_27_2017.py
@@ -0,0 +1,18 @@
+# If and while one in the same
+
+spam = 0
+if spam < 5:
+    print("Hello World.")
+    spam = + 1
+
+spam = 0
+while spam < 5:
+    print("Hello,world!")
+    spam = spam + 1
+
+spam = 5
+if spam < 5:
+    print("Hi World!")
+    spam = + 1
+else:
+    print("What??")
